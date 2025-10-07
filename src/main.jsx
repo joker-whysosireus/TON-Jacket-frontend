1bf88870246c03ba692318232cbe2a135acd1cad
joker-whysosireus <7kp2x@atomicmail.io>
2025-10-07 22:02:45 +0300

    fix wallet

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import {TonConnectUIProvider} from "@tonconnect/ui-react"
 
-const manifestUrl = 'https://github.com/joker-whysosireus/manifest-info-tonjacket/blob/main/manifest.txt';
+const manifestUrl = 'https://raw.githubusercontent.com/joker-whysosireus/manifest-info-tonjacket/main/manifest.txt';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
